dad2ae6b79f8d5d7748f4234f4baf97d01ccbe04
Sanhamm <sanhamm097@gmail.com>
2022-10-12 13:43:50 +0200

    Ferdig med js kravene

--- script/main.js
+++ script/main.js
@@ -1,15 +1,9 @@
 const API_BASE_URL = 'https://nf-api.onrender.com';
-
-//password:"heiheihei"
-//username:"kasomskjer@stud.noroff.no"
-
 const outPost = document.getElementById("outPost")
 
 async function getWhitToken(url) {
     try {
-        //console.log(url);
         const token = localStorage.getItem('accessToken');
-        //console.log(token);
         const fetchData = {
             method: 'GET',
             headers: {
@@ -19,6 +13,7 @@ async function getWhitToken(url) {
         };
         const response = await fetch(url, fetchData)
         const json = await response.json()
+        filterList = json
         ListPosts(json);
 
     } catch(error) {
@@ -29,7 +24,6 @@ async function getWhitToken(url) {
 const postsUrl = `${API_BASE_URL}/api/v1/social/posts/?_author=true`;
 
 const ListPosts = (post) => {
-    console.log(post);
     outPost.innerHTML = ""
     for(let inn of post){
         const deleteBtn = `<button class="btn btn-outline-primary my-2 btn-sm my-sm-0  deleteBtn"  data-delete="${inn.id}">Delete</button>`
@@ -59,27 +53,14 @@ const ListPosts = (post) => {
 
     for (let btnDelete of buttonDelete) {
         btnDelete.addEventListener("click", () => {
-        console.log(btnDelete.getAttribute("data-delete"));
         if (confirm("you want to delete this post?")) {
             deletePosts(btnDelete.getAttribute("data-delete"));
             }
     });
-}
-    const editButton = document.querySelectorAll("button.editBtn")
-
-    for ( let btnEdit of editButton) {
-        btnEdit.addEventListener("click", () => {
-            console.log("hei");
-
-        })
     }
-
-
 }
-
 
 async function deletePosts(id) {
-    console.log(id);
     const url = `${API_BASE_URL}/api/v1/social/posts/${id}`
     try {
         const token = localStorage.getItem("accessToken")
@@ -92,39 +73,12 @@ async function deletePosts(id) {
         };
         const response = await fetch(url, deleteData);
         const json = await response.json()
-        console.log(json);
         if (response.status === 200) window.location = "../index.html";
 
     } catch(error) {
         console.log(error);
     }
 }
-
-async function editPosts(id) {
-    console.log(id);
-    const url = `${API_BASE_URL}/api/v1/social/posts/${id}`
-    try {
-        const token = localStorage.getItem("accessToken")
-        const editData = {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${token}`,
-            },
-        };
-        const response = await fetch(url, editData)
-        const json = await response.json()
-        console.log(json);
-
-    } catch(error) {
-    console.log(error);
-    }
-}
-
-
-
-
-
 
 getWhitToken(postsUrl);
 
@@ -151,7 +105,6 @@ async function submittPost(url) {
             body: JSON.stringify(userToPost),
         };
         const response = await fetch(url, postData)
-        console.log(response);
         const json = await response.json();
         document.location.reload();
         titlePost = "";
@@ -167,9 +120,28 @@ btnPost.addEventListener('click', postFunction)
 
 function postFunction(event) {
     event.preventDefault();
-    console.log(submittPost());
     submittPost(postsUrl);
 }
+
+const search = document.getElementById("searchFilter")
+search.addEventListener("keyup", filterFunction)
+
+function filterFunction () {
+    const filterQuery = search.value.toLowerCase();
+
+    const filtering = filterList.filter((json) => {
+        const user = json.author.name;
+        const title = json.title
+        const body = json.body
+
+        if(user.indexOf(filterQuery) > -1) return true;
+        if(title.indexOf(filterQuery) > -1) return true;
+        if(body.indexOf(filterQuery) > -1) return true;
+        return false;
+    })
+        ListPosts(filtering)
+    }
+
 
 
 
