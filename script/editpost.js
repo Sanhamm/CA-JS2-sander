eebf2bae5b3cfb174c8f1f6f3d1b43fbf9f2156d
Sanhamm <sanhamm097@gmail.com>
2022-10-17 11:34:22 +0200

    Finnished edit
    now u can see the value of what you are suppose to edit.

--- script/editpost.js
+++ script/editpost.js
@@ -5,6 +5,46 @@ const params = new URLSearchParams(queryString);
 const id = params.get('id');
 
 editUrl = `${API_BASE_URL}/api/v1/social/posts/${id}`
+const outEdit = document.getElementById("edit")
+
+async function getWhitToken(url) {
+    try {
+        const token = localStorage.getItem('accessToken');
+        const fetchData = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+        };
+        const response = await fetch(url, fetchData)
+        const json = await response.json()
+        filterList = json
+        ListPosts(json);
+
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+const ListPosts = (post) => {
+    outEdit.innerHTML = ""
+        let newDiv = ` 
+        <div class="form-group col-sm-10 col-lg-8 mx-auto mb-5">
+            <label for="innlegg"></label>
+            <input type="text" id="title" value="${post.title}" class="form-control mb-4 border border-primary">
+            <textarea value="${post.body}" class="form-control mb-4 border border-primary" id="inlegg" style="resize: none;" rows="5">${post.body}</textarea>
+        </div>
+        `;
+        outEdit.innerHTML += newDiv;
+    }
+
+
+
+getWhitToken(editUrl);
+
+
+
 
 async function editPost(url) {
     const titleEdit = document.getElementById('title').value;
